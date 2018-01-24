24b49640ca1944da4a49b671ab06e734d1110c0c
Joseph Handschu <jhandschu@gmail.com>
2018-01-24 13:45:51 -0500

    Query wrapper
    Using a function to wrap the query
    
    ::where('completed',1)->get();
    
    will be able to use the route App\Task::incomplete();

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    // adding a wrapper query
+
+    public static function incomplete()
+    {
+    	return static::where('completed',0)->get();
+    }
 }
